5aeb085be7e43ab48a856df650a69af438b268ba
boris <boris021@mail.ru>
2022-09-11 14:59:31 +0500

    home

--- dist/js/app.js
+++ dist/js/app.js
@@ -3536,6 +3536,29 @@
                 if (countFiles) label.querySelector(".field__file-fake").innerText = "Selected files: " + countFiles; else label.querySelector(".field__file-fake").innerText = labelVal;
             }));
         }));
+        function setCookie(name, value, days) {
+            let expires = "";
+            if (days) {
+                let date = new Date;
+                date.setTime(date.getTime() + 24 * days * 60 * 60 * 1e3);
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        }
+        function getCookie(name) {
+            let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)"));
+            return matches ? decodeURIComponent(matches[1]) : void 0;
+        }
+        function checkCookies() {
+            let cookieNote = document.getElementById("cookie_note");
+            let cookieBtnAccept = cookieNote.querySelector(".cookie_accept");
+            if (!getCookie("cookies_policy")) cookieNote.classList.add("show");
+            cookieBtnAccept.addEventListener("click", (function() {
+                setCookie("cookies_policy", "true", 365);
+                cookieNote.classList.remove("show");
+            }));
+        }
+        checkCookies();
         window["FLS"] = true;
         isWebp();
         formFieldsInit();
